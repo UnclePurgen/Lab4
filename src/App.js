a26ca32791d2b67c38d2e85f7cae590eb8878dc1
UnclePurgen <71026196+UnclePurgen@users.noreply.github.com>
2023-12-27 20:46:55 +0300

    feat: Added semantic-release

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 
 function App() {
   const greeting = "Это тестовый проект для 6 лабораторной работы Стрельцова Артёма Владимировича";
+  const rel = "Added semantic-release"
   return (
     <div className="App">
      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
          {greeting}
       </header>
+      <div>
+        {rel}
+      </div>
     </div>
   );
 }
